e1884bbdb28ae3949eb8b737640de6fa60b6c404
tangcx <383258210@qq.com>
2018-08-05 23:21:49 +0800

    添加服务

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+ 
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res) {
+    
+    res.sendfile("./public/index.html");
+  });
+app.listen(8080, function() {
+ console.log('App listening at port 8080;');
+});
